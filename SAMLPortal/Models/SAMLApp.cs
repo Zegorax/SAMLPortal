5bcd18d53615fed82b86e21e211025510abae558
Luca Verardo <luca.verardo@pindex.ch>
2020-03-18 16:02:33 +0100

    Added base SAMLApp class

---
+++ SAMLPortal/Models/SAMLApp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace SAMLPortal.Models
+{
+    public class SAMLApp
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsEnabled { get; set; }
+        public string Secret { get; set; }
+
+        public SAMLApp()
+        {
+        }
+    }
+}
