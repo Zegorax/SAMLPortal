298e5e7c77bd73874f0db55703c925328f7aaff8
Luca Verardo <luca.verardo@pindex.ch>
2020-03-18 13:31:40 +0100

    Added base project

---
+++ SAMLPortal.Tests/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace SAMLPortal.Tests
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void TestCase()
+        {
+        }
+    }
+}
