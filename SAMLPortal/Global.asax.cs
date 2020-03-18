5b946b270bf12c6e6025b21d58a7fc6f46cdb1cb
Luca Verardo <luca.verardo@pindex.ch>
2020-03-18 13:31:40 +0100

    Added base project

---
+++ SAMLPortal/Global.asax.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Http;
+
+namespace SAMLPortal
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
